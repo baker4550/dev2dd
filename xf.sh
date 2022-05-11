ba56a4d7562027ff1af6bdceeab06bc065baf7fe
baker4550 <95163172+baker4550@users.noreply.github.com>
2022-05-11 11:24:16 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "bc6ebb82-bb38-4d0c-beed-19ce3e051d5b",
                         "alterId": 0
                     }
                 ],
